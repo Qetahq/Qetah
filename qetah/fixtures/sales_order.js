c9709346e619abbcfb6e80897596bffd1bba8335
noxer <qetahq@gmail.com>
2024-06-01 16:40:47 +0000

    Initial commit new changes

--- qetah/fixtures/sales_order.js
+++ qetah/fixtures/sales_order.js
@@ -34,13 +34,108 @@ frappe.ui.form.on('Sales Order Item', {
 		}
 		frappe.model.set_value(cdt, cdn, "custom_normal_qty",child.qty - child.custom_urgent_qty)
 	},
-	custom_urgent_rate:function(frm,cdt,cdn)
+	custom_is_this_urgent: function(frm, cdt, cdn)
 	{
 		var child = locals[cdt][cdn];
-		var cust_rate = child.custom_urgent_qty * child.custom_urgent_rate
-		var final_rate = child.rate + cust_rate
-		frappe.model.set_value(cdt, cdn, "price_list_rate",final_rate)	
-		frappe.model.set_value(cdt, cdn, "base_price_list_rate",final_rate)	
-		frappe.model.set_value(cdt, cdn, "rate",final_rate)	
-	}
+		frappe.model.set_value(cdt, cdn, "custom_urgent_qty", 0);
+		frappe.model.set_value(cdt, cdn, "custom_urgent_rate", 0);
+	},
+custom_urgent_rate: function(frm, cdt, cdn) {
+  let child = locals[cdt][cdn];
+
+  // Find all the "Urgent Cost" items for the current row
+  let urgent_cost_items = frm.doc.items.filter(function(item) {
+    return item.item_code === "Urgent Cost" && item.docname === child.name;
+  });
+
+  // Update the existing "Urgent Cost" item, if any
+  if (urgent_cost_items.length > 0) {
+    urgent_cost_items.forEach(function(item) {
+      frappe.model.set_value(item.doctype, item.name, "rate", child.custom_urgent_rate);
+      frappe.model.set_value(item.doctype, item.name, "qty", child.custom_urgent_qty);
+    });
+  } else {
+    // Find the index of the current row's item
+    let current_item_idx = frm.doc.items.findIndex(function(item) {
+      return item.name === child.name;
+    });
+
+    // Check if an "Urgent Cost" item already exists below the current row's item
+    let urgent_cost_item_below = frm.doc.items.find(function(item, index) {
+      return item.item_code === "Urgent Cost" && index > current_item_idx;
+    });
+
+    if (urgent_cost_item_below) {
+      // Update the existing "Urgent Cost" item below the current row's item
+      frappe.model.set_value(urgent_cost_item_below.doctype, urgent_cost_item_below.name, "rate", child.custom_urgent_rate);
+      frappe.model.set_value(urgent_cost_item_below.doctype, urgent_cost_item_below.name, "qty", child.custom_urgent_qty);
+    } else {
+      // Create a new "Urgent Cost" item and insert it after the current row's item
+      let new_item = frappe.model.add_child(frm.doc, "Sales Order Item", "items", current_item_idx + 2);
+      new_item.item_code = "Urgent Cost";
+      new_item.item_name = "Urgent Cost";
+      new_item.uom = "Nos";
+      new_item.rate = child.custom_urgent_rate;
+      new_item.qty = child.custom_urgent_qty;
+    }
+  }
+
+  // Refresh the item list
+  refresh_field("items");
+},
+
+custom_urgent_qty: function(frm, cdt, cdn) {
+  let child = locals[cdt][cdn];
+
+  // Find all the "Urgent Cost" items for the current row
+  let urgent_cost_items = frm.doc.items.filter(function(item) {
+    return item.item_code === "Urgent Cost" && item.docname === child.name;
+  });
+
+  // Update the existing "Urgent Cost" item, if any
+  if (urgent_cost_items.length > 0) {
+    urgent_cost_items.forEach(function(item) {
+      frappe.model.set_value(item.doctype, item.name, "rate", child.custom_urgent_rate);
+      frappe.model.set_value(item.doctype, item.name, "qty", child.custom_urgent_qty);
+    });
+  } else {
+    // Find the index of the current row's item
+    let current_item_idx = frm.doc.items.findIndex(function(item) {
+      return item.name === child.name;
+    });
+
+    // Check if an "Urgent Cost" item already exists below the current row's item
+    let urgent_cost_item_below = frm.doc.items.find(function(item, index) {
+      return item.item_code === "Urgent Cost" && index > current_item_idx;
+    });
+
+    if (urgent_cost_item_below) {
+      // Update the existing "Urgent Cost" item below the current row's item
+      frappe.model.set_value(urgent_cost_item_below.doctype, urgent_cost_item_below.name, "rate", child.custom_urgent_rate);
+      frappe.model.set_value(urgent_cost_item_below.doctype, urgent_cost_item_below.name, "qty", child.custom_urgent_qty);
+    } else {
+      // Create a new "Urgent Cost" item and insert it after the current row's item
+      let new_item = frappe.model.add_child(frm.doc, "Sales Order Item", "items", current_item_idx + 2);
+      new_item.item_code = "Urgent Cost";
+      new_item.item_name = "Urgent Cost";
+      new_item.uom = "Nos";
+      new_item.rate = child.custom_urgent_rate;
+      new_item.qty = child.custom_urgent_qty;
+    }
+  }
+
+  // Refresh the item list
+  refresh_field("items");
+}
+
+});
+
+frappe.ui.form.on('Sales Order', {
+    onload: function(frm) {
+        // Set the delivery date to the current date for new Sales Orders
+        if (frm.is_new()) {
+            const today = frappe.datetime.get_today();
+            frappe.model.set_value('Sales Order', frm.doc.name, 'delivery_date', today);
+        }
+    }
 });
---
+++ qetah/qetah/doctype/knlkmlk/test_knlkmlk.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, noxer and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testknlkmlk(FrappeTestCase):
+	pass
